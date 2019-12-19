e9ac881c9ae59b0e6b4c0fbe1eb3d118131c9091
Hatem Hamed Salem <hatem.salem@ihub.asu.edu.eg>
2019-12-19 11:07:43 +0200

    Initial Order components

---
+++ ilab-app/src/main/java/ilab/core/domain/LineItem.java
@@ -0,0 +1,32 @@
+package ilab.core.domain;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class LineItem extends AbstractEntity<LineItem>
+{
+	private int rank;
+	@ManyToOne(optional = false)
+	private OrderEntity orderEntity;
+	@ManyToOne(optional=false)
+	private Service service;
+	private String digtalAssets;
+	private long units;
+	private BigDecimal unitPrice;
+	@Temporal(TemporalType.DATE)
+	private Date plannedStardDate;
+	@Temporal(TemporalType.DATE)
+	private Date plannedEndDate;
+	@Temporal(TemporalType.DATE)
+	private Date estimatedStardDate;
+	@Temporal(TemporalType.DATE)
+	private Date estimatedEndDate;
+
+
+}
