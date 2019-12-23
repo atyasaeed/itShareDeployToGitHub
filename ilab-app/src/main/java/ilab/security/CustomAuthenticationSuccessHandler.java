4c41d633acfcd7ea9ee3389020fb153e0ecc8afa
Hatem Hamed Salem <hatem.salem@ihub.asu.edu.eg>
2019-12-23 13:55:35 +0200

    FBA implementation

---
+++ ilab-app/src/main/java/ilab/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,36 @@
+package ilab.security;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import ilab.core.domain.User;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler
+{
+
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException
+	{
+		response.setStatus(HttpStatus.OK.value());
+		Map<String, Object> data = new HashMap<>();
+//		response.getOutputStream().println(authentication);
+		User user=(User)authentication.getPrincipal();
+		response.getOutputStream().println(String.format("{\"name\":\"%s\"}", user.getUsername()));
+//		response.getOutputStream().println("Hello");
+		
+	}
+}
