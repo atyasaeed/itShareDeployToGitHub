b6d0f002ac381b342a92d64dcdf2352c7c1c4bd0
Hatem Hamed Salem <hatem.salem@ihub.asu.edu.eg>
2019-12-29 12:16:17 +0200

    refactoring

--- ilab-app/src/main/java/ilab/api/ServiceController.java
+++ ilab-app/src/main/java/ilab/api/ServiceController.java
@@ -3,14 +3,11 @@ package ilab.api;
 import java.util.Optional;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
