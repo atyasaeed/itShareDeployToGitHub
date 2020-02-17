9576efd44a4b1f3002114197ee5981f6f1519b00
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2020-02-17 12:36:57 +0200

    [BE] Add Search method for Service Endpoint with pagination,sorting & filteration

--- ilab-app/src/main/java/ilab/core/repository/ServiceRepository.java
+++ ilab-app/src/main/java/ilab/core/repository/ServiceRepository.java
@@ -5,18 +5,15 @@ import java.util.UUID;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ilab.core.domain.Service;
 
 @Repository
 @Transactional(value = TxType.SUPPORTS)
-public interface ServiceRepository extends PagingAndSortingRepository<Service, UUID>
+public interface ServiceRepository extends PagingAndSortingRepository<Service, UUID>,JpaSpecificationExecutor<Service>
 {
-	//TODO: just for testing
-	@Query("UPDATE Service s SET s.id = :newId WHERE s.id = :oldId")
-	void updateTestingData(@Param("oldId") UUID id1, @Param("newId") UUID id2);
+	
 }
