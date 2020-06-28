10abb451bd21e98e642f135cde8f6c0627f05a4f
Abdo13121994 <abdofcis2013@yahoo.com>
2020-06-29 00:25:07 +0200

    services to be cont

--- ilab-ui/src/app/layout/shopping-cart/shopping-cart-form/shopping-cart-form.component.ts
+++ ilab-ui/src/app/layout/shopping-cart/shopping-cart-form/shopping-cart-form.component.ts
@@ -4,12 +4,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ShoppingCartService } from '../shoppingcart.service';
 import { AuthenticationService } from 'src/app/shared/services';
 import { APP_CONFIG, IAppConfig } from 'src/app/shared/app.config';
-import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators, FormControl, AbstractControl } from '@angular/forms';
 import { ServicesService } from './services.service';
 import * as fromStore from 'src/app/store';
 import { Store } from '@ngrx/store';
 import { routerTransition } from 'src/app/router.animations';
-
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 @Component({
   selector: 'app-shopping-cart-form',
   templateUrl: './shopping-cart-form.component.html',
@@ -21,16 +21,54 @@ export class ShoppingCartFormComponent implements OnInit, AfterViewInit {
   services;
   loading = false;
   submitted = false;
-  ext: string[] = [];
-  extFile: string;
-  filename: string;
+  msgFileSize: any;
+  //ext: string[] = [];
+  //extFile: string;
+  //filename: string;
   // item: ShoppingCartItem = new ShoppingCartItem();
-  item: ShoppingCartItem;
+  //item: ShoppingCartItem;
   // fileToUpload: File = null;
-  public totalfiles: Array<File> = [];
-  public totalFileName = [];
-  public lengthCheckToaddMore = 0;
+  //public totalfiles: Array<File> = [];
+  //public totalFileName = [];
+  //public lengthCheckToaddMore = 0;
   service: Service;
+  //dropdownList = [];
+  //selectedItems = [];
+  dropdownSettings: IDropdownSettings = {};
+  activeTest: any = {};
+  test = {
+    services: [
+      {
+        id: '1',
+        name: '3D Printing',
+        description: '3D Printing Mono Color',
+        maxFiles: 1,
+        process: {
+          multi: true,
+          values: ['Cut', 'Engrave'],
+        },
+        materials: ['Mat3', 'Mat4'],
+        //thickness: ['1.0mm', '2.0mm'],
+        supportedExtensions: ['*.stl'],
+        type: ['type1', 'type2'],
+        color: ['color1', 'color2'],
+        unit: ['das', 'sda'],
+      },
+      {
+        id: '2',
+        name: 'Laser Cutting',
+        description: 'Laser Cutting',
+        maxFiles: 1,
+        process: {
+          multi: false,
+          values: ['SLA', 'FDA'],
+        },
+        materials: ['Mat1', 'Mat2'],
+        thickness: ['6.0mm', '5.0mm'],
+        supportedExtensions: ['*.stl'],
+      },
+    ],
+  };
   // tslint:disable-next-line: max-line-length
   constructor(
     private route: ActivatedRoute,
@@ -43,148 +81,206 @@ export class ShoppingCartFormComponent implements OnInit, AfterViewInit {
     private appStore: Store<fromStore.AppState>
   ) {}
 
-  cartForm: FormGroup;
+  form: FormGroup;
 
-  get cartForm$() {
-    return this.cartForm.controls;
-  }
-  get file$() {
-    return this.cartForm$.files as FormArray;
-  }
+  // get cartForm$() {
+  //   return this.cartForm.controls;
+  // }
+  // get file$() {
+  //   return this.cartForm$.files as FormArray;
+  // }
   ngOnInit() {
     this.createForm();
     this.appStore.select(fromStore.getAuthServices).subscribe((res) => {
       // this.services = new Array(res);
       this.services = Object.assign(res);
       this.service = this.services[0];
-      this.bulidForm(this.service);
+      //this.bulidForm(this.service);
     });
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      //idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true,
+    };
   }
   ngAfterViewInit() {}
 
   createForm() {
-    this.cartForm = this.formBuilder.group({
-      quantity: ['1', [Validators.required, Validators.minLength(1)]],
-      attend: [''],
-      startingDate: [''],
-      deliveryDate: [''],
-      notes: ['', [Validators.maxLength(250)]],
-
-      files: new FormArray([]),
+    this.form = this.formBuilder.group({
+      quantity: ['1', [Validators.required, Validators.min(1), this.numberValidator]],
+      // attend: [''],
+      // startingDate: [''],
+      // deliveryDate: [''],
+      notes: [''],
+      file: ['', Validators.required],
+      // files: new FormArray([]),
+      materials: ['undefined', [Validators.required]],
+      width: ['', [Validators.required, this.numberValidator]],
+      height: ['', [Validators.required, this.numberValidator]],
     });
   }
 
-  onSubmit() {
-    this.loading = true;
-    this.submitted = true;
-    console.log(this.cartForm.value);
-    if (this.cartForm.invalid) {
-      this.validateAllFormFields(this.cartForm);
-      this.loading = false;
-      return;
+  numberValidator(control: AbstractControl): { [key: string]: any } | null {
+    //console.log(control.value);
+    //const value: string = control.value || '';
+    if (control.value == null) {
+      return { NaN: true };
     }
-    this.item = Object.assign(this.cartForm.value);
-    this.item.service = {} as Service;
-    this.item.service.id = this.service.id;
-    for (let index = 0; index < this.file$.length; index++) {
-      // this.item.files.push(this.file$[index]);
-      this.item.files;
-    }
-
-    const formData: FormData = new FormData();
-    for (let j = 0; j < this.totalfiles.length; j++) {
-      // formData.append(this.totalFileName[j] + (j + 1), <File>this.totalfiles[j]);
-      formData.append('files', this.totalfiles[j] as File);
-    }
-    // formData.append('file', this.fileToUpload, this.fileToUpload.name);
-    const itemBlob = new Blob([JSON.stringify(this.item)], {
-      type: 'application/json',
-    });
-
-    formData.append('item', itemBlob);
-
-    console.log(this.file$.value);
-    for (let index = 0; index < this.file$.value.length; index++) {
-      console.log(this.file$.value[index].file);
-    }
-    this.shoppingCartService.create(formData).subscribe(
-      (resp) => {
-        this.appStore.dispatch(new fromStore.LoadInitState());
-        this.router.navigateByUrl('shopping-cart'), (this.loading = false);
-      },
-      (err) => (this.loading = false)
-    );
+    return null;
   }
+  // onItemSelect(item: any) {
+  //   console.log(item);
+  // }
+  // onSelectAll(items: any) {
+  //   console.log(items);
+  // }
 
-  getImageUrl(): string {
-    return this.appConfig.ASSETS_URL + this.service.id;
-  }
+  // onSubmit() {
+  //   this.loading = true;
+  //   this.submitted = true;
+  //   //console.log(this.cartForm.value);
+  //   if (this.cartForm.invalid) {
+  //     this.validateAllFormFields(this.cartForm);
+  //     this.loading = false;
+  //     return;
+  //   }
+  //   this.item = Object.assign(this.cartForm.value);
+  //   this.item.service = {} as Service;
+  //   this.item.service.id = this.service.id;
+  //   for (let index = 0; index < this.file$.length; index++) {
+  //     // this.item.files.push(this.file$[index]);
+  //     this.item.files;
+  //   }
 
-  msgFileSize: any;
+  //   const formData: FormData = new FormData();
+  //   for (let j = 0; j < this.totalfiles.length; j++) {
+  //     // formData.append(this.totalFileName[j] + (j + 1), <File>this.totalfiles[j]);
+  //     formData.append('files', this.totalfiles[j] as File);
+  //   }
+  //   // formData.append('file', this.fileToUpload, this.fileToUpload.name);
+  //   const itemBlob = new Blob([JSON.stringify(this.item)], {
+  //     type: 'application/json',
+  //   });
 
-  handleFileInput(fileInput: any, oldIndex) {
-    //  this.filename= fileInput.target.files[0].name;
-    this.msgFileSize = fileInput.target.files[0].size;
-    if (this.msgFileSize > this.appConfig.FILE_SIZE) {
-      return;
-    }
+  //   formData.append('item', itemBlob);
 
-    if (fileInput.target.files && fileInput.target.files[0]) {
-      let reader = new FileReader();
-      reader.onload = (event: any) => {};
-      if (oldIndex == 0) {
-        this.totalfiles.unshift(fileInput.target.files[0]);
-        this.totalFileName.unshift(fileInput.target.files[0].name);
-      } else {
-        this.totalfiles[oldIndex] = fileInput.target.files[0];
-        this.totalFileName[oldIndex] = fileInput.target.files[0].name;
-      }
+  //   //console.log(this.file$.value);
+  //   for (let index = 0; index < this.file$.value.length; index++) {
+  //     //console.log(this.file$.value[index].file);
+  //   }
+  //   this.shoppingCartService.create(formData).subscribe(
+  //     (resp) => {
+  //       this.appStore.dispatch(new fromStore.LoadInitState());
+  //       this.router.navigateByUrl('shopping-cart'), (this.loading = false);
+  //     },
+  //     (err) => (this.loading = false)
+  //   );
+  // }
 
-      reader.readAsDataURL(fileInput.target.files[0]);
-    }
+  // getImageUrl(): string {
+  //   return this.appConfig.ASSETS_URL + this.service.id;
+  // }
 
-    if (this.totalfiles.length == 1) {
-      this.lengthCheckToaddMore = 1;
-    }
-  }
-  onChangeObj(event) {
-    console.log(event);
-  }
+  // handleFileInput(fileInput: any, oldIndex) {
+  //   //  this.filename= fileInput.target.files[0].name;
+  //   this.msgFileSize = fileInput.target.files[0].size;
+  //   if (this.msgFileSize > this.appConfig.FILE_SIZE) {
+  //     return;
+  //   }
+
+  //   if (fileInput.target.files && fileInput.target.files[0]) {
+  //     let reader = new FileReader();
+  //     reader.onload = (event: any) => {};
+  //     if (oldIndex == 0) {
+  //       this.totalfiles.unshift(fileInput.target.files[0]);
+  //       this.totalFileName.unshift(fileInput.target.files[0].name);
+  //     } else {
+  //       this.totalfiles[oldIndex] = fileInput.target.files[0];
+  //       this.totalFileName[oldIndex] = fileInput.target.files[0].name;
+  //     }
+
+  //     reader.readAsDataURL(fileInput.target.files[0]);
+  //   }
+
+  //   if (this.totalfiles.length == 1) {
+  //     this.lengthCheckToaddMore = 1;
+  //   }
+  // }
+
+  // onChangeObj(event) {
+  //   //console.log(event);
+  // }
   bulidForm(event) {
-    console.log(event);
-    this.file$.clear();
-    this.service = event;
-    for (let i = 0; i < this.service.maxFiles; i++) {
-      this.file$.push(
-        this.formBuilder.group({
-          file: ['', [Validators.required]],
-          material: [this.service?.materials[0]?.name, [Validators.required]],
-          type: [this.service?.materials[0]?.types[0]?.name, [Validators.required]],
-          color: [this.service.materials[0].types[0].colors[0], [Validators.required]],
-          dimension: [this.service.materials[0].types[0].dimensions[0], [Validators.required]],
-          unit: [this.service.units[0], [Validators.required]],
-        })
-      );
-    }
-
-    for (let index = 0; index < this.service?.supportedExtensions?.length; index++) {
-      this.ext.push(this.service.supportedExtensions[index].substr(1));
-    }
-    this.extFile = this.ext.toString();
-  }
-
-  validateAllFormFields(formGroup: FormGroup) {
-    // {1}
-    Object.keys(formGroup.controls).forEach((field) => {
-      // {2}
-      const control = formGroup.get(field); // {3}
-      if (control instanceof FormControl) {
-        // {4}
-        control.markAsTouched({ onlySelf: true });
-      } else if (control instanceof FormGroup) {
-        // {5}
-        this.validateAllFormFields(control); // {6}
-      }
+    console.log(event.target.value);
+    this.form.reset({
+      quantity: 1,
     });
+    this.form.markAsUntouched();
+    this.form.markAsPristine();
+    this.activeTest = this.test.services.find((e) => e.id === event.target.value);
+    //   this.file$.clear();
+    console.log(this.activeTest);
+    if (this.activeTest.thickness != undefined) {
+      this.form.addControl('thickness', new FormControl('', Validators.required));
+    } else {
+      this.form.removeControl('thickness');
+    }
+    if (this.activeTest.color != undefined) {
+      this.form.addControl('color', new FormControl('', Validators.required));
+    } else {
+      this.form.removeControl('color');
+    }
+    if (this.activeTest.type != undefined) {
+      this.form.addControl('type', new FormControl('', Validators.required));
+    } else {
+      this.form.removeControl('type');
+    }
+    if (this.activeTest.process != undefined) {
+      this.form.addControl('process', new FormControl('', Validators.required));
+    } else {
+      this.form.removeControl('process');
+    }
+
+    //   this.service = event;
+    //   for (let i = 0; i < this.service.maxFiles; i++) {
+    //     this.file$.push(
+    //       this.formBuilder.group({
+    //         file: ['', [Validators.required]],
+    //         material: [this.service?.materials[0]?.name, [Validators.required]],
+    //         type: [this.service?.materials[0]?.types[0]?.name, [Validators.required]],
+    //         color: [this.service.materials[0].types[0].colors[0], [Validators.required]],
+    //         dimension: [this.service.materials[0].types[0].dimensions[0], [Validators.required]],
+    //         unit: [this.service.units[0], [Validators.required]],
+    //       })
+    //     );
+    //   }
+    //   for (let index = 0; index < this.service?.supportedExtensions?.length; index++) {
+    //     this.ext.push(this.service.supportedExtensions[index].substr(1));
+    //   }
+    //   this.extFile = this.ext.toString();
   }
+
+  submit() {
+    console.log(this.form);
+  }
+
+  // validateAllFormFields(formGroup: FormGroup) {
+  //   // {1}
+  //   Object.keys(formGroup.controls).forEach((field) => {
+  //     // {2}
+  //     const control = formGroup.get(field); // {3}
+  //     if (control instanceof FormControl) {
+  //       // {4}
+  //       control.markAsTouched({ onlySelf: true });
+  //     } else if (control instanceof FormGroup) {
+  //       // {5}
+  //       this.validateAllFormFields(control); // {6}
+  //     }
+  //   });
+  // }
 }
