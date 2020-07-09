851bcc0bfd69b92918a6f0543296dd316918c42a
Abdo13121994 <abdofcis2013@yahoo.com>
2020-07-09 23:15:06 +0200

    service tree updated

--- ilab-ui/src/app/layout/services-list/service-form/service-form.component.ts
+++ ilab-ui/src/app/layout/services-list/service-form/service-form.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-service-form',
   templateUrl: './service-form.component.html',
-  styleUrls: ['./service-form.component.scss']
+  styleUrls: ['./service-form.component.scss'],
 })
 export class ServiceFormComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  @ViewChild('stepTwo') stepTwo: ElementRef;
+  @ViewChild('stepOne') stepOne: ElementRef;
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(null),
+      description: new FormControl(null),
+      image: new FormControl(null),
+      supportedExtensions: new FormArray([new FormControl()]),
+    });
   }
 
+  ngAfterViewInit() {
+    this.stepTwo.nativeElement.style.display = 'none';
+  }
+
+  addControlArray(type: string) {
+    const control = new FormControl(null);
+    (<FormArray>this.form.get(type)).push(control);
+  }
+
+  uploadImage(event) {
+    console.log(event.target.files[0]);
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
+  next() {
+    this.stepOne.nativeElement.style.display = 'none';
+    this.stepTwo.nativeElement.style.display = 'block';
+  }
+
+  addAttribute(event, type: string) {
+    console.log(event.target.checked);
+    if (event.target.checked) {
+      this.form.addControl(type, new FormArray([new FormControl()]));
+    } else {
+      this.form.removeControl(type);
+    }
+  }
 }
--- ilab-ui/src/app/layout/services-list/services-list.module.ts
+++ ilab-ui/src/app/layout/services-list/services-list.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ServicesListComponent } from './services-list.component';
 import { ServicesListRoutingModule } from './services-list-routing.module';
 import { ServiceFormComponent } from './service-form/service-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ServicesListComponent, ServiceFormComponent],
-  imports: [CommonModule, ServicesListRoutingModule],
+  imports: [CommonModule, ServicesListRoutingModule, ReactiveFormsModule],
 })
 export class ServicesListModule {}
