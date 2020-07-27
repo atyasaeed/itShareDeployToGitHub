f7e170746c0b4365e2bc39ff5075b07e0fa5e674
Abdo13121994 <abdofcis2013@yahoo.com>
2020-07-27 16:43:12 +0200

    lazy loading preload update

--- ilab-ui/src/app/layout/layout-routing.module.ts
+++ ilab-ui/src/app/layout/layout-routing.module.ts
@@ -58,6 +58,7 @@ const routes: Routes = [
       {
         path: 'services-list',
         loadChildren: () => import('./services-list/services-list.module').then((m) => m.ServicesListModule),
+        canActivate: [AdminGuard],
       },
     ],
   },
--- ilab-ui/src/app/shared/services/custom-preloading-strategy.service.ts
+++ ilab-ui/src/app/shared/services/custom-preloading-strategy.service.ts
@@ -9,15 +9,15 @@ export class CustomPreloadingStrategy implements PreloadingStrategy {
   preload(route: Route, loadMe: () => Observable<any>): Observable<any> {
     if (route.data && route.data['preload']) {
       var delay: number = route.data['delay'];
-      console.log('preload called on ' + route.path + ' with a delay of ' + delay);
+      //console.log('preload called on ' + route.path + ' with a delay of ' + delay);
       return timer(delay).pipe(
         flatMap((_) => {
-          console.log('Loading now ' + route.path + ' module');
+          //console.log('Loading now ' + route.path + ' module');
           return loadMe();
         })
       );
     } else {
-      console.log('no preload for the path ' + route.path);
+      //console.log('no preload for the path ' + route.path);
       return of(null);
     }
   }
