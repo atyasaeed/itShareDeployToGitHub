f0c3d85da9be937b7d02a3449258b5dbb5801e93
Abdo13121994 <abdofcis2013@yahoo.com>
2020-08-10 00:48:08 +0200

    stl model added

--- ilab-ui/src/app/layout/shopping-cart/shopping-cart.component.ts
+++ ilab-ui/src/app/layout/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,6 @@
 import { ToastrService } from 'ngx-toastr';
 import { User } from '../../shared/domain/user.model';
-import { Component, OnInit, Inject, ElementRef } from '@angular/core';
+import { Component, OnInit, Inject, ElementRef, TemplateRef } from '@angular/core';
 import { ShoppingCartItem, Order, LineItem } from 'src/app/shared/domain';
 import { ShoppingCartService } from '../../shared/services/shoppingcart.service';
 import { Router } from '@angular/router';
@@ -10,12 +10,15 @@ import * as fromStore from 'src/app/store';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { routerTransition } from 'src/app/router.animations';
-
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { debounce, debounceTime, switchMap, delay } from 'rxjs/operators';
 import * as THREE from 'three/build/three.module.js';
 import { TdLoadingService } from '@covalent/core/loading';
 import { GalleryService } from 'src/app/shared/services/gallery.service';
+import { animate } from '@angular/animations';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
@@ -35,7 +38,22 @@ export class ShoppingCartComponent extends DefaultListComponent<ShoppingCartItem
   dropdownSettings: IDropdownSettings = {};
   // quantitiesCount;
   selectedItemsArray;
-  test = true;
+  //test = true;
+  modalRef: BsModalRef;
+  stlColorBtns = [
+    'aqua',
+    'orange',
+    'blue',
+    'black',
+    'silver',
+    'brown',
+    'red',
+    'purple',
+    'pink',
+    'olive',
+    'gold',
+    'green',
+  ];
   //test: string[] = ['assets/images/teapot.stl'];
   // items: Array<ShoppingCartItem>;
   constructor(
@@ -44,7 +62,8 @@ export class ShoppingCartComponent extends DefaultListComponent<ShoppingCartItem
     private toastr: ToastrService,
     @Inject(APP_CONFIG) public appConfig: IAppConfig,
     loadingService: TdLoadingService,
-    private galleryService: GalleryService
+    private galleryService: GalleryService,
+    private modalService: BsModalService
   ) {
     super(service, loadingService);
     this.authUser$ = this.appStore.select(fromStore.getAuthUser);
@@ -84,6 +103,8 @@ export class ShoppingCartComponent extends DefaultListComponent<ShoppingCartItem
       this.hasAdminRole = user && user.roles.includes('ROLE_ADMIN');
     });
   }
+
+  ngAfterViewInit() {}
 
   delete(entity) {
     this.purge(entity).subscribe((result) => {
@@ -242,5 +263,88 @@ export class ShoppingCartComponent extends DefaultListComponent<ShoppingCartItem
   onDeSelectAll(element: HTMLElement) {
     //console.log(this.selectedItemsArray[i]);
     element.innerText = '*processes is required';
+  }
+
+  STLViewer(model, elementID) {
+    var elem = document.getElementById(elementID);
+    var camera = new THREE.PerspectiveCamera(70, elem.clientWidth / elem.clientHeight, 1, 1000);
+    var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(elem.clientWidth, elem.clientHeight);
+    elem.appendChild(renderer.domElement);
+    window.addEventListener(
+      'resize',
+      function () {
+        renderer.setSize(elem.clientWidth, elem.clientHeight);
+        camera.aspect = elem.clientWidth / elem.clientHeight;
+        camera.updateProjectionMatrix();
+      },
+      false
+    );
+    var controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.rotateSpeed = 0.05;
+    controls.dampingFactor = 0.1;
+    controls.enableZoom = true;
+    controls.autoRotate = true;
+    controls.autoRotateSpeed = 2;
+    document.getElementById('toggleRotate').addEventListener('click', function () {
+      controls.autoRotate = !controls.autoRotate;
+    });
+
+    var scene = new THREE.Scene();
+    scene.add(new THREE.HemisphereLight(0xffffff, 1.5));
+    new STLLoader().load(model, (geometry) => {
+      var material = new THREE.MeshPhongMaterial({
+        color: 0x87d,
+        specular: 100,
+        shininess: 100,
+      });
+      var mesh = new THREE.Mesh(geometry, material);
+
+      scene.add(mesh);
+      mesh.setColor = function (color) {
+        mesh.material.color.set(color);
+      };
+      //var colorbtn = ['red', 'green'];
+      this.stlColorBtns.forEach((e) => {
+        document.getElementById(e).addEventListener('click', function () {
+          mesh.setColor(e);
+        });
+      });
+      //mesh.material.smoothShading = true;
+
+      document.getElementById('toggleWireFrame').addEventListener('change', function (e: any) {
+        if (e.target.checked) {
+          mesh.material.wireframe = true;
+        } else {
+          mesh.material.wireframe = false;
+        }
+      });
+
+      var middle = new THREE.Vector3();
+      geometry.computeBoundingBox();
+      geometry.boundingBox.getCenter(middle);
+      mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(-middle.x, -middle.y, -middle.z));
+      var largestDimension = Math.max(
+        geometry.boundingBox.max.x,
+        geometry.boundingBox.max.y,
+        geometry.boundingBox.max.z
+      );
+      camera.position.z = largestDimension * 3;
+      var animate = function () {
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene, camera);
+      };
+      animate();
+    });
+  }
+
+  loadStl(path: string, template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+    if (document.getElementById('model').childNodes[0]) {
+      document.getElementById('model').removeChild(document.getElementById('model').childNodes[0]);
+    }
+    this.STLViewer(path, 'model');
   }
 }
