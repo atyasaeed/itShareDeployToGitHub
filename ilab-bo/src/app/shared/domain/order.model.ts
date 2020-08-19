2ec1b82b37a9badaf43c7454eca53ededfd7d782
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2020-08-19 15:37:55 +0200

    Renaming the projects and the folders to the standard convention

--- ilab-bo/src/app/shared/domain/order.model.ts
+++
@@ -1,9 +0,0 @@
-import { OrderItem } from './order-item.model';
-
-export class Order {
-  id: string;
-  date: Date;
-  total_payment: number;
-  status: string;
-  lineItems: OrderItem[];
-}
--- ilab-bo/src/app/shared/domain/shoppingcart-item.model.ts
+++
@@ -1,9 +0,0 @@
-import { LineItem } from './line-item.model';
-
-export class ShoppingCartItem extends LineItem {
-  constructor() {
-    super();
-    // this.unitPrice=180;
-    this.quantity = 1;
-  }
-}
