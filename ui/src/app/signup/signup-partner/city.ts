6a3c0abd765ec72033b61a7f0400ee7d3e085530
iLab-Abdelrahman <58903079+iLab-Abdelrahman@users.noreply.github.com>
2020-08-23 11:02:52 +0200

    fix -issue

---
+++ ui/src/app/signup/signup-partner/city.ts
@@ -0,0 +1,30 @@
+export interface City {
+  name: string;
+}
+
+export const cities: City[] = [
+  {
+    name: 'Cairo',
+  },
+  {
+    name: 'Alexandria',
+  },
+  {
+    name: 'Aswan',
+  },
+  {
+    name: 'Asyut',
+  },
+  {
+    name: 'Beheira',
+  },
+  {
+    name: 'Beni Suef',
+  },
+  {
+    name: 'Dakahlia',
+  },
+  {
+    name: 'Damietta',
+  },
+];
--- ui/src/app/signup/signup-partner/signup-partner.component.ts
+++ ui/src/app/signup/signup-partner/signup-partner.component.ts
@@ -11,6 +11,7 @@ import * as fromStore from 'src/app/store';
 import { Store } from '@ngrx/store';
 import { routerTransition } from 'src/app/router.animations';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { City, cities } from './city';
 
 @Component({
   selector: 'app-signup-partner',
@@ -25,6 +26,7 @@ export class SignupPartnerComponent implements OnInit {
   dropdownList: Service[] = [];
   selectedItems = [];
   dropdownSettings: IDropdownSettings = {};
+  cities: City[] = cities;
 
   constructor(
     private router: Router,
@@ -90,17 +92,18 @@ export class SignupPartnerComponent implements OnInit {
 
   ngOnInit(): void {
     this.createForm();
+    // this.registrationForm.get('services').patchValue([]);
   }
 
   createForm() {
     this.registrationForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(80)]],
       // currentPosition: ['', [Validators.required]],
       mobileNo: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
       city: ['', [Validators.required]],
       address: ['', [Validators.required]],
-      website: [''],
-      services: [''],
+      website: ['', [Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')]],
+      services: [null],
     });
   }
 
