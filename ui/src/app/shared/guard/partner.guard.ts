43e5b39c1343fedd2ec8793793518d62a9174814
mo <muhammadelkhateb95@gmail.com>
2020-09-23 14:05:11 +0200

    Get BE

--- ui/src/app/shared/guard/partner.guard.ts
+++ ui/src/app/shared/guard/partner.guard.ts
@@ -3,6 +3,8 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Rout
 import { Observable } from 'rxjs';
 import { User } from '../domain';
 import { UserService } from '../services/user.service';
+import * as fromStore from 'src/app/store';
+import { Store } from '@ngrx/store';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +14,24 @@ export class PartnerGuard implements CanActivate {
 
   private isPartner = false;
   user = {} as User;
-  constructor(private router: Router, private service: UserService) {
-    this.service.getAuthUserDetails().subscribe((res) => {
-      // this.isAuthenticated = res !== null;
-      // this.isPartner = res && res.defaultOrg.type === 'PARTNER';
-      // console.log(this.isPartner);
-      console.log(res);
-      if (res.defaultOrg.type === 'PARTNER') {
+  constructor(private router: Router, private service: UserService, private appStore: Store<fromStore.AppState>) {
+    // this.service.getAuthUserDetails().subscribe((res) => {
+    //   // this.isAuthenticated = res !== null;
+    //   // this.isPartner = res && res.defaultOrg.type === 'PARTNER';
+    //   // console.log(this.isPartner);
+    //   console.log(res);
+    //   if (res.defaultOrg.type === 'PARTNER') {
+    //     this.isPartner = true;
+    //   }
+    // });
+
+    appStore.select(fromStore.getAuthUser).subscribe((user) => {
+      // this.isAuthenticated = user !== null;
+      // this.hasPrivilege = user && (user.roles.includes('ROLE_REGISTER_PRIVILEGE') || user.roles.length == 0);
+      // this.user = user;
+
+      console.log(user);
+      if (user.defaultOrg.type === 'PARTNER') {
         this.isPartner = true;
       }
     });
