acd0611973096527556ad999288b83930ef8a853
iLab-Abdelrahman <58903079+iLab-Abdelrahman@users.noreply.github.com>
2020-10-15 17:25:09 +0200

    show notes for admin,alert message and update partner signup

--- ui/src/app/layout/organization-team/organization-team.component.ts
+++ ui/src/app/layout/organization-team/organization-team.component.ts
@@ -63,7 +63,7 @@ export class OrganizationTeamComponent extends DefaultListComponent<OrgUser, Org
     this._dialogService
       .openConfirm({
         title: this.translate.instant('areYouSure'),
-        message: this.translate.instant('removeMemeber'),
+        message: this.translate.instant('deleteDescription'),
         cancelButton: this.translate.instant('no'),
         acceptButton: this.translate.instant('yes'),
         width: '500px',
@@ -77,7 +77,7 @@ export class OrganizationTeamComponent extends DefaultListComponent<OrgUser, Org
             },
             (err) => {
               if (err.error.details[0] === 'Owner can not delte himself') {
-                this.toastr.error(this.translate.instant('ownerCanNotDelteHimself'));
+                this.toastr.error(this.translate.instant('ownerCanNotDeleteHimself'));
               }
             }
           );
